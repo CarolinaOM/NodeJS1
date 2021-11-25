bcabc1b2c0c01936b191b31c7f6fc5b12d51e0b4
CarolinaOM <caro13081991@gmail.com>
2021-11-25 21:03:02 +0000

    Conocer las varibales en ES5 y ES6

--- primera-app/app.js
+++ primera-app/app.js
@@ -1,5 +1,13 @@
-function saludo (nombre){
-    console.log('Hola ' +nombre);
-}
+//ES5 - Variables
+var nombre = "Caro";
+console.log(nombre);
+nombre = "Isa";
+console.log(nombre);
 
-saludo('Caro');
+//ES6+ - Variables
+const nombre6 = "Migui"; // no se puede cambiar la varible
+let edad = 24;
+console.log(nombre6);
+console.log(edad);
+edad = 30;
+console.log(edad);
