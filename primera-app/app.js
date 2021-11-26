8206458d238d0ff03175db839b0b8a8d79350b75
CarolinaOM <caro13081991@gmail.com>
2021-11-26 12:44:39 +0000

    Conocer funciones Callback

--- primera-app/app.js
+++ primera-app/app.js
@@ -1,4 +1,4 @@
-//ES5 - Variables
+/*//ES5 - Variables
 var nombre = "Caro";
 console.log(nombre);
 nombre = "Isa";
@@ -10,4 +10,51 @@ let edad = 24;
 console.log(nombre6);
 console.log(edad);
 edad = 30;
-console.log(edad);
+console.log(edad);*/
+
+
+
+//Funciones flecha
+//Funciones en ES5
+
+/*const years = [2000, 2005, 2008, 2012];
+
+var edad5 = years.map(function(el){
+    return 2021 - el;
+})
+console.log(edad5);
+
+//Funciones en ES6 de tipo felcha
+let edad6 = years.map((el) => {
+    return 2019 - el;
+});
+console.log(edad6);
+//Otra forma
+edad6 = years.map(el => 2019 - el);
+console.log(edad6);*/
+
+
+
+//Funciones Callback
+function Mensaje (callback){
+    console.log('Mensaje antes de la llamada callback. ');
+    callback();
+}
+function Saludo (){
+    console.log('Saludo despues de la llamada al calback. ');
+}
+Mensaje(Saludo);
+//Otro ejemplo
+function Sumar (num1, num2, callback){
+    let Resultado = num1 + num2;
+    callback(Resultado);
+}
+function Resultado(res){
+    console.log(res);
+}
+Sumar(5, 8,  Resultado);
+//otra
+setTimeout(function (){
+console.log('Esto se va a ejecutar despues de 3 segundos');
+}, 3000);
+
