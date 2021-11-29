157d6e7b3f8c365935debe82fcadd4a6d66c9a4d
CarolinaOM <caro13081991@gmail.com>
2021-11-29 14:31:47 +0000

    Modulo OS

--- primera-app/Modulos/app.js
+++ primera-app/Modulos/app.js
@@ -5,8 +5,17 @@ dato('Hola mundo.');
 //console.log(module);*/
 
 
-const path = require('path');
+/*const path = require('path');
 
 const objPath = path.parse(__filename);
 
-console.log(objPath.name);
+console.log(objPath.name);*/
+
+
+const os = require('os');
+
+let memoriaLibre = os.freemem();
+let memoriaTotal = os.totalmem();
+
+console.log(`Memoria Liobre: ${memoriaLibre}`);
+console.log(`emoria Total: ${memoriaTotal}`);
