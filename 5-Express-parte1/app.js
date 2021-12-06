14dd32ca944f210a95f1d781bac9e8d6f774b17f
CarolinaOM <caro13081991@gmail.com>
2021-12-06 17:38:10 +0000

    manejo solicitudes HTTP GET y POST

--- 5-Express-parte1/app.js
+++ 5-Express-parte1/app.js
@@ -1,5 +1,14 @@
+const { json } = require('express');
 const express = require ('express');
 const app = express();
+
+app.use(express,json);
+
+const usuarios = [
+    {id:1, nombre:'Caro'},
+    {id:2, nombre:'Mari'},
+    {id:3, nombre:'Ana'}
+];
 
 app.get('/', (req, res) => {
     res.send('Hola Mundo dede Express.');
@@ -9,8 +18,18 @@ app.get('/api/usuarios', (req, res) => {
     res.send(['caro', 'maria', 'ana'])
 });
 
-app.get('/api/usuarios/:years/:mes', (req, res)=>{
-    res.send(req.query);
+app.get('/api/usuarios/:id', (req, res)=>{
+    let usuario = usuarios.find(u => u.id === parseInt(req.params.id));
+    if(!usuario) res.status(404).send('El usuario no fue encoentrado');
+    res.send(usuario);
+});
+
+app.post('/api/usuarios',(req, res)=> {
+    const usuario = {
+        id: usuarios.length + 1,
+        nombre: req.body.nombre
+    };
+    usuarios.push(usuarios);
 });
 
 const port = process.env.PORT ||3000;
