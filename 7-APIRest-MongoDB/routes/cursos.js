1a0c46bbbf6e07acbdf45f55d828c7c159cf121a
CarolinaOM <caro13081991@gmail.com>
2021-12-16 15:29:37 +0000

    POST - Registrar curso

--- 7-APIRest-MongoDB/routes/cursos.js
+++ 7-APIRest-MongoDB/routes/cursos.js
@@ -1,8 +1,31 @@
 const express = require('express');
+const Curso = require ('../models/curso_model');
 const ruta = express.Router();
 
 ruta.get('/', (req, res) => {
     res.json('Listo el GER de cursos.');
 });
+
+ruta.post('/', (req, res)=>{
+    let resultado = crearCurso (req.body);
+
+    resultado.then(curso => {
+        res.json({
+            curso
+        }).catch(err => {
+            res.status(400).json({
+                err
+            })
+        })
+    })
+})
+
+async function crearCurso(body){
+    let curso = new Curso ({
+        titulo     : body.titulo,
+        descripcion : body.desc,
+    });
+    return await curso.save();
+}
 
 module.exports = ruta;
