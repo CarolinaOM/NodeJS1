69a6ac65a173f4f08922ca72a3018e47f2f9e1ed
CarolinaOM <caro13081991@gmail.com>
2021-12-16 16:40:34 +0000

    PUT - Actualizar datos del curso

--- 7-APIRest-MongoDB/routes/cursos.js
+++ 7-APIRest-MongoDB/routes/cursos.js
@@ -20,12 +20,31 @@ ruta.post('/', (req, res)=>{
     })
 })
 
+ruta.put('/:id', (req, res) => {
+    let resultado = actualizarCurso(req.params.id, req.body);
+    resultado.then(curso => {
+        res.json(curso)
+    }).catch(err => {
+        res.status(400).json(err)
+    })
+})
+
 async function crearCurso(body){
     let curso = new Curso ({
         titulo     : body.titulo,
         descripcion : body.desc,
     });
     return await curso.save();
+}
+
+async function actualizarCurso(id, body){
+    let curso = await Curso.findByIdAndUpdate(id, {
+        $set: {
+            titulo: body.titulo,
+            descripcion: body.desc
+        }
+    }, {new: true});
+    return curso;
 }
 
 module.exports = ruta;
