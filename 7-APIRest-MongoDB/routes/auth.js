0e2e93d550b4eb67f32525ccee584dad78777abd
CarolinaOM <caro13081991@gmail.com>
2021-12-18 21:49:46 +0000

    Login de usuario - parte 2

--- 7-APIRest-MongoDB/routes/auth.js
+++ 7-APIRest-MongoDB/routes/auth.js
@@ -8,6 +8,8 @@ ruta.post('/', (req, res) => {
     Usuario.findOne({email: req.body.email})
     .then(datos => {
         if(datos){
+            const passwordValido = bcrypt.compareSync(req.body.password, datos.password);
+            if(!passwordValido) return res.status(400).json({error:'ok', msj:'Usuario o contraseña incorrecta.'})
             res.json(datos);
         }else{
             res.status(400).json({
